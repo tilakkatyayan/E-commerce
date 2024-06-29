bfc73be67e1eb50b2dba17690ccb43d075cbb936
Tilak <katyayantilak@gmail.com>
2024-06-29 12:39:15 +0530

    third commit

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+
+import { ShopContext } from "./../Context/ShopContext";
+
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "./../Components/ProductDisplay/ProductDisplay";
+
 
 const Product = () => {
-  return (
-    <div>
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
 
-    </div>
-  )
-}
+    const product = all_product.find((e) => e.id === Number(productId));
 
-export default Product
+    return (
+        <div>
+            <Breadcrum product={product} />
+            <ProductDisplay product={product} />
+            
+        </div>
+    );
+};
+
+export default Product;
